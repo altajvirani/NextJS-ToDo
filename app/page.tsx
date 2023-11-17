0d56cef58ef378854a8215136e298587d4cc4d04
altajvirani <altajvirani786@gmail.com>
2023-11-17 13:08:55 +0530

    Trying new viewport units

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ToDo from "./components/ToDo";
 
 export default function App() {
   return (
-    <main className="w-screen h-screen px-6 sm:px-6 xl:px-10 2xl:px-12 flex items-center justify-center bg-slate-100">
+    <main className="w-screen h-[100dvh] px-6 sm:px-6 xl:px-10 2xl:px-12 flex items-center justify-center bg-slate-100">
       <ToDo />
     </main>
   );
